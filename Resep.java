a7a5b0cf45e8da0869b3d4759431734ac6a944bf
cahyonoyonoidm2019@gmail.com <cahyonoyonoidm2019>
2022-10-06 11:25:08 +0700

    Plankton mencury krabby patty

---
+++ Resep.java
@@ -0,0 +1,10 @@
+public class Resep {
+    public static void main(String[] args) {
+        System.out.println("Resep Krabby Patty");
+        System.out.println("--------------------------------");
+        System.out.println("1. Roti");
+        System.out.println("2. Daging");
+        System.out.println("3. Sawi");
+        System.out.println("4. Tomat");
+    }
+}
